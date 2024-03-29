3b414a8559e1991cf2e3247c9c91686754785109
Leonardo Soares <leonardofs88@live.com>
2024-03-29 15:06:18 +0000

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Leonardo Soares on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
